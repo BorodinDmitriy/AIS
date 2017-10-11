79d78411af3a3dba7ddee228a0743b9f2da88a54
Дмитрий <>
2017-10-11 16:59:24 +0300

    imageProcessor begin

---
+++ Lab1/Lab1/ImageProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Lab1
+{
+    class ImageProcessor
+    {
+        private int sizeX;
+        private int sizeY;
+        private Bitmap image;
+        private Network net;
+
+        public ImageProcessor(Bitmap processingImage, Network workingNetwork)
+        {
+            sizeX = image.Width;
+            sizeY = image.Height;
+            image = processingImage;
+            net = workingNetwork;
+        }
+        public void processImage()
+        {
+            
+        }
+    }
+}
