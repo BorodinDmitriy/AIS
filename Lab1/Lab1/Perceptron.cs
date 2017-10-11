bb17615c29fdb4aca7e0e1c8355dbe8eae122750
Pavel <catkiller95@gmail.com>
2017-10-11 16:34:45 +0300

    измененый перцептрон

--- Lab1/Lab1/Perceptron.cs
+++ Lab1/Lab1/Perceptron.cs
@@ -8,10 +8,12 @@ namespace Lab1
 {
     class Perceptron
     {
+        public string name;
         public int[,] scale_signal;
         public int[,] weight;
         public int[,] input;
-        public int limit = 9;
+        public int[,] memory;
+        public double result;
         public int sum;
         private int sizeX;
         private int sizeY;
@@ -21,8 +23,10 @@ namespace Lab1
             weight = new int[sizex, sizey];
             input = new int[sizex, sizey];
             scale_signal = new int[sizex, sizey];
+            memory = new int[sizex, sizey];
             sizeX = sizex;
             sizeY = sizey;
+            result = 0;
             return;
         }
 
