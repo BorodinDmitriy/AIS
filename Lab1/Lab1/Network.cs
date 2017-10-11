4533b4def5146d771e5f7997d1c2db79e7da03f0
Pavel <catkiller95@gmail.com>
2017-10-11 16:31:37 +0300

    Сеть нейноров

---
+++ Lab1/Lab1/Network.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Network
+    {
+        private Perceptron[] percept;
+        public Network(int sizex, int sizey)
+        {
+            percept = new Perceptron[10];
+            for (int i = 0; i < 10; i++)
+            {
+                percept[i] = new Perceptron(sizex, sizey);
+            }
+        }
+    }
+}
