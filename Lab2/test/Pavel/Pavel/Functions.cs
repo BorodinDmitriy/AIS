c5b05e2e2086991d8c3e157f4f0230061df5e546
Дмитрий <>
2017-12-14 23:12:37 +0300

    добавил класс функций

---
+++ Lab2/test/Pavel/Pavel/Functions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pavel
+{
+    public class Functions
+    {
+        Random randObj;
+        double val;
+        public Functions(double value)
+        {
+            randObj = new Random();
+            val = value;
+        }
+
+        public double budgetFunction(double x)
+        {
+            return Math.Pow(val * randObj.NextDouble(), 1.1) * x + Math.Pow(Math.Sin(x), 7); 
+        }
+
+        public double flatFunction(double y)
+        {
+            return Math.Pow(Math.Sqrt(val) * randObj.NextDouble(), 0.32) * y;
+        }
+
+        public double transportFunction(double z)
+        {
+            return Math.Pow(Math.Sqrt(val) * randObj.NextDouble(), 0.05) * Math.Pow(z,1.04);
+        }
+    }
+}
