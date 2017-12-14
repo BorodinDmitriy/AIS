8108563d712a2f23bcefebe6b7d250bef6e91cd3
Qoolerus <gmod2007@gmail.com>
2017-12-14 22:57:48 +0300

    Добавлен еще один класс фузификации.

---
+++ Lab2/test/Pavel/Pavel/FuzzificationCarSpendPercept.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pavel
+{
+    class FuzzificationCarSpendPercept
+    {
+        private double _c = 0;
+        private double _gamma = 0;
+
+        public double c
+        {
+            get { return _c; }
+            set { _c = value; }
+        }
+
+        public double gamma
+        {
+            get { return _gamma; }
+            set { _gamma = value; }
+        }
+
+        public FuzzificationCarSpendPercept(double c, double gamma)
+        {
+            this._c = c;
+            this._gamma = gamma;
+        }
+
+        public double Transform(double x)
+        {
+            double res = Math.Exp(-1 * Math.Pow((x - this._c) / this._gamma, 2));
+            return res;
+        }
+    }
+}
