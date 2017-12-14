403eab5b213e481cc96e353beaa619df9ad42588
Pavel <catkiller95@gmail.com>
2017-12-14 20:38:02 +0300

    Тест приложение для network

---
+++ Lab2/test/Pavel/Pavel/Network.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pavel
+{
+    class Network
+    {
+        private List<FuzzificationIncomePercept> IncomePecept;
+        private List<Fuzzification>
+    }
+}
